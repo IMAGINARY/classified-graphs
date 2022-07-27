IMAGINARY/classified-graphs

commit 60abfc1b56f9f6989385fe4c6bd3f6b70264f798

Add Clear mode

src/ts/main.ts
@@ -27,6 +27,7 @@ import ModeExport from './ModeExport';
 import ModeImport from './ModeImport';
 import ModeLoad from './ModeLoad';
 import ModeLayout from './ModeLayout';
+import ModeClear from './ModeClear';
 
 import * as assets from './assets';
 
@@ -90,10 +91,10 @@ function main() {
 
   const toolbarModes: ModeConfig[] = [
     {
-      modeName: 'modeNull',
-      textKey: 'Pointer',
-      icon: assets.iconPointer,
-      modeObj: new ModeNull(cy, parameters),
+      modeName: 'modeClear',
+      textKey: 'Clear',
+      icon: assets.iconClear,
+      modeObj: new ModeClear(cy, parameters),
     },
     {
       modeName: 'modeExport',
@@ -112,6 +113,12 @@ function main() {
       textKey: 'Load',
       icon: assets.iconLoad,
       modeObj: new ModeLoad(cy, parameters),
+    },
+    {
+      modeName: 'modeNull',
+      textKey: 'Pointer',
+      icon: assets.iconPointer,
+      modeObj: new ModeNull(cy, parameters),
     },
     {
       modeName: 'modeNode',

src/img/trash3.svg
@@ -1,0 +1,3 @@
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+  <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
+</svg>

src/ts/ModeClear.ts
@@ -1,0 +1,27 @@
+/* eslint-disable class-methods-use-this */
+import { Core } from 'cytoscape';
+import { Mode, Parameters } from './modes';
+
+export default class ModeClear implements Mode {
+  cy;
+
+  parameters;
+
+  constructor(cy: Core, parameters: Parameters) {
+    this.cy = cy;
+    this.parameters = parameters;
+  }
+
+  activate = () => {
+    this.cy.elements().remove();
+    this.cy.emit('cm-graph-updated');
+  };
+
+  render() {}
+
+  infobox(): string {
+    return '';
+  }
+
+  deactivate = () => {};
+}

src/ts/assets.ts
@@ -30,6 +30,7 @@ const iconImport = new URL('../img/box-arrow-in-down.svg', import.meta.url)
   .href;
 const iconExport = new URL('../img/box-arrow-down.svg', import.meta.url).href;
 const iconLoad = new URL('../img/folder2-open.svg', import.meta.url).href;
+const iconClear = new URL('../img/trash3.svg', import.meta.url).href;
 
 export {
   iconPointer,
@@ -41,4 +42,5 @@ export {
   iconImport,
   iconExport,
   iconLoad,
+  iconClear,
 };

src/ts/locales/en.ts
@@ -7,6 +7,7 @@ const en: Locale = {
     translation: {
       title: 'Graph explorer',
       hello: 'Hello world',
+      Clear: 'Clear',
       Pointer: 'Pointer',
       Export: 'Export',
       Import: 'Import',

src/ts/locales/locale.ts
@@ -5,6 +5,7 @@ type Locale = {
     translation: Partial<{
       title: string;
       hello: string;
+      Clear: string;
       Pointer: string;
       Export: string;
       Import: string;

commit 61c73636d65148f6abba8e95eab0d3b23ab6ba03

Fix error introduced in 60abfc1
The re-ordering of the toolbar items made the modeClear the default,
instead of modeNull.

src/ts/main.ts
@@ -191,7 +191,7 @@ function main() {
     },
   ];
 
-  const modeNull = toolbarModes[0].modeObj;
+  const modeNull = new ModeNull(cy, parameters);
   let primaryMode: Mode = modeNull;
   let secondaryMode: Mode = infoboxModes[0].modeObj;
 